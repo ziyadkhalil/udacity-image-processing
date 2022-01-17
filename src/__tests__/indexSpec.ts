0d5133f3095f4c33bb3750077a32b9eeca9cdabb
Ziyad Khalil <ziyadhkhalil@gmail.com>
2022-01-17 20:51:00 +0200

    testing

--- src/__tests__/indexSpec.ts
+++ src/__tests__/indexSpec.ts
@@ -1,3 +1,16 @@
-it("2 + 2 = 4", () => {
-  expect(2 + 2).toBe(4);
+import supertest from "supertest";
+
+import app from "..";
+
+const request = supertest(app);
+
+it("File name is required", async () => {
+  const res = await request.get("/api/images");
+  expect(res.status).toBe(400);
+  expect(res.text).toBe("File name is required");
+});
+
+it("Gets image if it exists", async () => {
+  const res = await request.get("/api/images?fileName=fjord");
+  expect(res.status).toBe(200);
 });
